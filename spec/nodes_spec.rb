953cee06e8d8b2927a27d9eef3fde94da0131384
Tyler Christiansen <code@tylerc.me>
2016-01-03 16:16:11 -0800

    Update RSpec.
    Add test for GET /nodes/:hostname.  It should return a single node.

--- spec/nodes_spec.rb
+++ spec/nodes_spec.rb
@@ -26,3 +26,30 @@ describe "Getting a list of nodes" do
     expect(last_response).to be_ok
   end
 end
+
+describe "Getting a single node" do
+  let(:expected) {
+    {
+      name: "rtr1.example.com",
+      last: {
+        start: "2016-01-03T03:22:27Z",
+        end: "2016-01-03T03:22:29Z",
+        status: "success",
+        time: 1.516074609
+      },
+      group: "default",
+      full_name: "default/rtr1.example.com",
+      vars: {}
+    }
+  }
+  before do
+    get '/nodes/rtr1.example.com'
+  end
+  let(:response) { JSON.parse(last_response.body, symbolize_names: true) }
+  it "should return a list of nodes" do
+    expect(response).to eq(expected)
+  end
+  it "should be successful" do
+    expect(last_response).to be_ok
+  end
+end
