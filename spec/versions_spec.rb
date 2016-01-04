86f0f32cb5add2037e7eac9685db7fb2c873f0b0
Tyler Christiansen <code@tylerc.me>
2016-01-03 20:05:45 -0800

    Fix expected return and route for versions spec.
    Change expected return to an array of objects.
    Change hostname to `rtr1.example.com` to be similar to `nodes_spec.rb`.
    Update route to reflect current route (with a group).
    See supertylerc/oxidized-web#13

--- spec/versions_spec.rb
+++ spec/versions_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "Getting a version for a single node" do
   let(:expected) {
-    {
+    [{
       commit: {
         hash: "9e890265f00a7369e25e7ef2de92e5f94a65a0ab",
         date: "2016-01-02T20:06:13Z",
@@ -11,12 +11,12 @@ describe "Getting a version for a single node" do
           name: "oxidized",
           date: "2016-01-02T20:06:13Z"
         },
-        message: "update 192.168.33.2"
+        message: "update rtr1.example.com"
       }
-    }
+    }]
   }
   before do
-    get '/versions/rtr1.example.com'
+    get '/versions/default/rtr1.example.com'
   end
   let(:response) { JSON.parse(last_response.body, symbolize_names: true) }
   it "should return a version for a node" do
