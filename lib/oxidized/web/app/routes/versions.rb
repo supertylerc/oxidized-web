b6422445386f97c55f8d955223347f72bf18c4d8
Tyler Christiansen <code@tylerc.me>
2016-01-09 15:03:42 -0800

    Add route for latest version for a node.
    Uses the `#last` method to grab the last version appended to the list of
    versions.
    See supertylerc/oxidized-web#17

--- lib/oxidized/web/app/routes/versions.rb
+++ lib/oxidized/web/app/routes/versions.rb
@@ -8,6 +8,11 @@ module Oxidized
           @data = Models::Versions.new! params[:hostname], params[:group]
           @data.versions.to_json
         end
+
+        get '/versions/:group/:hostname/latest' do
+          @data = Models::Versions.new! params[:hostname], params[:group]
+          @data.versions.last.to_json
+        end
       end
     end
   end
