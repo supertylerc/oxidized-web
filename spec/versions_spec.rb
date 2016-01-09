2455b0f9c5faffaf78abaaca04da65af3cc24393
Tyler Christiansen <code@tylerc.me>
2016-01-09 15:15:19 -0800

    Add test for proposed `/versions` route.
    Add an RSpec test to validate `/versions`.
    See supertylerc/oxidized-web#16.

--- spec/versions_spec.rb
+++ spec/versions_spec.rb
@@ -56,3 +56,35 @@ describe 'Getting the latest version for a node' do
     expect(last_response).to be_ok
   end
 end
+
+describe 'Getting a list of versions for all nodes' do
+  let(:expected) do
+    {
+      :'rtr1.example.com' => [{
+        commit: {
+          hash: '9e890265f00a7369e25e7ef2de92e5f94a65a0ab',
+          date: '2016-01-02T20:06:13Z',
+          author: {
+            email: 'oxidized@tylerc.me',
+            name: 'oxidized',
+            date: '2016-01-02T20:06:13Z'
+          },
+          message: 'update rtr1.example.com'
+        }
+      }]
+    }
+  end
+
+  before do
+    get '/versions'
+  end
+
+  let(:response) { JSON.parse(last_response.body, symbolize_names: true) }
+  it 'should return a list of versions for all nodes' do
+    expect(response).to eq(expected)
+  end
+
+  it 'should be successful' do
+    expect(last_response).to be_ok
+  end
+end
