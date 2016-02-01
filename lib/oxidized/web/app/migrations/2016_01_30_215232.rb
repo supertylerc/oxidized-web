dba567f0cfb4ec0d187921df8dbbe7b3f49b0dbe
Tyler Christiansen <code@tylerc.me>
2016-01-31 16:02:54 -0800

    Add first migration for Users
    See supertylerc/oxidized-web#35

---
+++ lib/oxidized/web/app/migrations/2016_01_30_215232.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+migration "Create Users Database" do
+  database.create_table :users do
+    primary_key :id
+    text :first_name
+    text :last_name
+    text :client_id
+    text :client_secret
+    integer :privilege_level, null: false
+    timestamp :created_at, null: false
+
+    index :client_id, unique: true
+  end
+end
