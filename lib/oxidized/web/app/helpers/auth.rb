f8ab941bfe28c0f88148d533177a772ad2fc5b6c
Tyler Christiansen <code@tylerc.me>
2016-01-31 16:57:33 -0800

    Simplify auth helper
    Add method to handle the auth header to simplify complexity
    
    See supertylerc/oxidized-web#35

--- lib/oxidized/web/app/helpers/auth.rb
+++ lib/oxidized/web/app/helpers/auth.rb
@@ -4,18 +4,21 @@ module Oxidized
   module Web
     module Helpers
       def valid_key?
-        halt 403 unless env['HTTP_AUTHORIZATION']
-        auth_header = env['HTTP_AUTHORIZATION'].split(':')
-        client_id = auth_header[0]
-        signature = auth_header[1]
-        client = Models::Users.first(client_id: client_id)
+        client = Models::Users.first(client_id: auth_header[:client_id])
         halt 403 if client.nil?
-        data = request.path
-        error 401 unless sign(client[:client_secret], data) == signature
+        error 401 unless sign(client[:client_secret], request.path) == auth_header[:signature]
       end
 
       def sign(client_secret, data)
         OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), client_secret, data)
+      end
+
+      private
+
+      def auth_header
+        halt 403 unless env['HTTP_AUTHORIZATION']
+        auth_header = env['HTTP_AUTHORIZATION'].split(':')
+        { client_id: auth_header[0], signature: auth_header[1] }
       end
     end
   end
